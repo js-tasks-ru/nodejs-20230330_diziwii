db58a41778528caf46d5e1206a9c15d0e1646045
Dmitriy Maslennikov <dmaslennikov@ostrovok.ru>
2023-04-11 01:33:14 +0300

    added error text

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,7 +1,7 @@
 function sum(...args) {
   return args.reduce((acc, num) => {
     if (typeof num !== 'number') {
-      throw new TypeError();
+      throw new TypeError('arg not a number');
     }
 
     return acc + num;
