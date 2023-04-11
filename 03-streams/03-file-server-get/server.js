fb4201c44fe16d7f8c7c774633b091d300779d59
Dmitriy Maslennikov <dmaslennikov@ostrovok.ru>
2023-04-12 01:22:29 +0300

    fix

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,19 +1,19 @@
-const url = require("url");
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
+const url = require('url');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
-server.on("request", (req, res) => {
+server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, "files", pathname);
+  const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
-    case "GET":
-      if (pathname.split("/").length > 1) {
+    case 'GET':
+      if (pathname.split('/').length > 1) {
         res.statusCode = 400;
         res.end();
       }
@@ -27,12 +27,12 @@ server.on("request", (req, res) => {
 
       fileStream.pipe(res);
 
-      fileStream.on("error", () => {
+      fileStream.on('error', () => {
         res.statusCode = 500;
         res.end();
       });
 
-      req.on("close", () => {
+      req.on('close', () => {
         fileStream.destroy();
       });
 
@@ -40,7 +40,7 @@ server.on("request", (req, res) => {
 
     default:
       res.statusCode = 501;
-      res.end("Not implemented");
+      res.end('Not implemented');
   }
 });
 
